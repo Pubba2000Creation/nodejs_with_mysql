3ea6ef1dee4a4660ca73b39e7fba12a42ab43872
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-06-15 16:14:48 +0530

    install body parser and import it to app.js and use it as the middelware

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // Import the Express library
 const express = require('express');
+//import body-parser to projrct
+const bodyParser =require('body-parser')
 
 // Create an instance of an Express application
 const app = express();
 
 // Import the post routes from the 'posts' module in the 'routes' directory
 const postRouter = require('./routes/posts');
+
+//to use it as the middelware
+app.use(bodyParser.json());
+
 
 // Use the imported post routes for any requests to the "/posts" URL path
 // This means that any request to "/posts" will be handled by the 'postRouter'
