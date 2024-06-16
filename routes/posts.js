6d3acfda14dbfc52bed09c8a7597ad03d5692a18
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-06-16 17:01:06 +0530

    modyfiy post controller

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,7 @@ const postController = require('../controllers/post.controller');
 // Create a new router object to define routes
 const router = express.Router();
 
+
 // Define a GET endpoint for the root URL ("/") and associate it with the 'index' function from postController
 //
 router.post("/", postController.save);
