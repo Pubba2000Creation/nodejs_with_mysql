af5a99b2a9576685793bb52e6a1de6b6cd9ab528
prabod pubudu peiris <89020751+Pubba2000Creation@users.noreply.github.com>
2024-06-24 16:29:59 +0530

    impement the file uploader for separete api multer is used for handling file uploads. path is used for working with file and directory paths. destination specifies the folder where uploaded files will be stored. filename generates a unique filename by appending the current timestamp to the original file's extension. This function checks the MIME type of the uploaded file. It accepts only JPEG and PNG files and rejects others with an error

--- helpers/image-uploader.js
+++ helpers/image-uploader.js
@@ -1,3 +1,54 @@
-// for the image uploadign functionaly hrer using pakage call mullter it can
-// upload any kind of file into our project
+const multer = require("multer"); // Importing multer for handling file uploads
+const path = require("path"); // Importing path to work with file and directory paths
 
+// Configuring the storage settings for multer
+const storage = multer.diskStorage({
+
+    // Setting the destination folder for uploaded files
+    destination: function(req, file, callback) {
+
+        callback(null, './uploads'); // All uploaded files will be saved in the 'uploads' directory
+    },
+
+    // Setting the filename for uploaded files
+    filename: function(req, file, callback) {
+
+        // Creating a unique filename by appending the current timestamp to the original file extension
+        callback(null, new Date().getTime() + path.extname(file.originalname));
+    }
+});
+
+// File filter to accept only image files (JPEG and PNG)
+const fileFilter = (req, file, callback) => {
+
+    // Checking the file's MIME type to ensure it's an image
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+
+        callback(null, true); // Accept the file
+        
+    } else {
+
+        // Reject the file and pass an error message
+        callback(new Error('Unsupported file format. Only JPEG and PNG are allowed.'), false);
+    }
+}
+
+// Configuring multer with storage settings, file size limit, and file filter
+const upload = multer({
+
+    storage: storage, // Using the configured storage settings
+
+    limits: {
+        fileSize: 1024 * 1024 * 20 // Limiting the file size to 20MB
+    },
+
+    fileFilter: fileFilter // Using the configured file filter
+
+});
+
+// Exporting the upload function for use in other parts of the application
+module.exports = {
+
+    upload: upload
+    
+}
